1332b47b248cb28277a00b96b8f2ac8f5050fe8f
JB <sjbh3mc@gmail.com>
2023-08-01 18:16:36 +0900

    Feat) make Test Folder and Test Script

---
+++ 2D project/Assets/Test/Test.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace sjb
+{
+    public class Test : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("Hello1");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Debug.Log("Hello2");
+        }
+    }
+}
