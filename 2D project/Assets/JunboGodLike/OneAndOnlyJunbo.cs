697aaa769c62eb0c1684f76ff8980a8548c18d13
onasheep <94965545+onasheep@users.noreply.github.com>
2023-08-01 17:38:32 +0900

    Feat : add JunBoGodLike folder and scripts

---
+++ 2D project/Assets/JunboGodLike/OneAndOnlyJunbo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneAndOnlyJunbo : MonoBehaviour
+{
+    private string name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        name = "JunBo";
+        name += "is God";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
