97dbb7ed218a5ef90474a0ca38f726fc28bfad89
sh12250 <99242620+sh12250@users.noreply.github.com>
2023-08-01 17:33:26 +0900

    Add. MakeShimGreater Script

---
+++ 2D project/Assets/Scripts/MakeShimGreater.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeShimGreater : MonoBehaviour
+{
+    string hisName = default;
+
+    private void Start()
+    {
+        hisName = "Shim";
+    }
+
+    private void Update()
+    {
+        if (hisName == "Shim")
+        {
+            hisName = "GodShim";
+        }
+
+        Debug.Log(name);
+    }
+}
