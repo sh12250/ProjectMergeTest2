a7a4dd4eabb591b9d91803d12ca0f3ec6b7c94da
sh12250 <99242620+sh12250@users.noreply.github.com>
2023-08-01 18:26:00 +0900

    Fix. Merge Conplict?

--- 2D project/Assets/Test/Test.cs
+++ 2D project/Assets/Test/Test.cs
@@ -2,48 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD
+//<<<<<<< HEAD
 
-<<<<<<< HEAD
-namespace hmg
-=======
-namespace sjb
->>>>>>> origin/Shim2
-=======
+//<<<<<<< HEAD
+//namespace hmg
+//=======
+//namespace sjb
+//>>>>>>> origin/Shim2
+//=======
 namespace hsj
->>>>>>> origin/HongSungJaeZZang
+//>>>>>>> origin/HongSungJaeZZang
 {
     public class Test : MonoBehaviour
     {
         // Start is called before the first frame update
         void Start()
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
+            //<<<<<<< HEAD
+            //<<<<<<< HEAD
 
-=======
+            //=======
             Debug.Log("Hello1");
->>>>>>> origin/Shim2
-=======
+            //>>>>>>> origin/Shim2
+            //=======
 
->>>>>>> origin/HongSungJaeZZang
+            //>>>>>>> origin/HongSungJaeZZang
         }
 
         // Update is called once per frame
         void Update()
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
+            //<<<<<<< HEAD
+            //<<<<<<< HEAD
 
-=======
+            //=======
             Debug.Log("Hello2");
->>>>>>> origin/Shim2
+            //>>>>>>> origin/Shim2
         }
     }
-=======
+    //=======
 
-        }
-    }
+    //        }
+    //    }
 
->>>>>>> origin/HongSungJaeZZang
+    //>>>>>>> origin/HongSungJaeZZang
 }
